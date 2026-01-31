47f7b6ccd8db8eec0c4c2737827d44cbb7d4fb1d
Harsh <harshthite@gmail.com>
2026-02-01 00:01:09 +0530

    Day 03: Solved peakindex (LeetCode)

---
+++ Day03/peakindex.java
@@ -0,0 +1,19 @@
+public class peakindex {
+    class Solution {
+        public int peakIndexInMountainArray(int[] arr) {
+            int start = 0;
+            int end = arr.length - 1;
+
+            while (start < end) {
+                int mid = start + (end - start) / 2;
+
+                if (arr[mid] < arr[mid + 1]) {
+                    start = mid + 1;
+                } else {
+                    end = mid;
+                }
+            }
+            return start; // or end (both are same)
+        }
+    }
+}
