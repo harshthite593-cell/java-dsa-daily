5a18460f8571817d71f57314eca6a41c0b324387
Harsh <harshthite593@gmail.com>
2026-02-10 00:13:28 +0530

    Day 08:learned cyclicsort

--- Day 07/Insertionsort.java
+++ Day 07/Insertionsort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Insertionsort {
     static void main(String[] args) {
-        int[] arr = {5,3,4,1,2};
+        int[] arr = {5,3,4,0,0,-5};
           iinsertionsrot(arr);
         System.out.println(Arrays.toString(arr));
     }
---
+++ Day 08/Cyclicsort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Cyclicsort {
+    static void main(String[] args) {
+        int[] arr = {1,3,2,5,4,7,6};
+        cyclicsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicsort(int[] arr){
+        int i = 0;
+
+        while (i < arr.length) {
+            if(arr[i] != i + 1){
+                int no  = arr[i];
+                int ref = arr[no - 1];
+                arr[no - 1] = arr[i];
+                arr[i] = ref;
+            } else {
+                i++;
+            }
+        }
+    }
+
+}
